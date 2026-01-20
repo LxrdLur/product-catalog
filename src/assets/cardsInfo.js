81b627260a2f90389b7a33ba198671f78e9413f1
Nikita Parshin <Panda01r@mail.ru>
2026-01-20 15:52:41 +0300

    full redesign and also add file with cards info

--- src/assets/cardsInfo.js
+++ src/assets/cardsInfo.js
@@ -1,16 +1,27 @@
 export const products = [
-    {id: 1, price: 9999, type: 'Видеокарта', title: 'MSI GTX 1660',
+    {
+        id: 1, price: 9999, type: 'Видеокарта',
+        title: 'PCI-E MSI GeForce GTX 1660 Ti GAMING X 6144MB 192bit GDDR6 [GTX 1660 TI GAMING X 6G] [PCIe 3.0, GDDR6, 3 x DisplayPort, HDMI]',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tarchitecto blanditiis dicta dignissimos fugiat minima minus nesciunt placeat porro sint ',
+        tags: ['Видеокарта','MSI'],
         img: 'gtx1660.jpg'},
 
 
-    // {id: 2, price: 9999, type: 'Видеокарта', title: 'Nvidia RTX 4090',
-    //     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tarchitecto blanditiis dicta dignissimos fugiat minima minus nesciunt placeat porro sint ',
-    //     img: 'rtx4090.jpg'},
+    {
+        id: 2, price: 9999, type: 'Видеокарта',
+        title: 'GIGABYTE GeForce RTX 4090 GAMING OC [GV-N4090GAMING OC-24GD 1.1] [PCIe 4.0, GPU 2230 МГц, 24 ГБ GDDR6X, 384 бит, 3 x DisplayPort, HDMI]',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tarchitecto blanditiis dicta dignissimos fugiat minima minus nesciunt placeat porro sint ',
+        tags: ['Видеокарта','MSI'],
+        img: 'rtx4090.jpg'
+    },
 
 
-    // {id: 3, price: 9999, type: 'Видеокарта', title: 'Asus Radeon RX 7900',
-    //     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tarchitecto blanditiis dicta dignissimos fugiat minima minus nesciunt placeat porro sint ',
-    //     img: 'rx7900.jpg'}
+    {
+        id: 3, price: 9999, type: 'Видеокарта',
+        title: 'ASUS AMD Radeon RX 7900 XTX TUF GAMING OC Edition [TUF-RX7900XTX-O24G-GAMING] [PCIe 4.0, GPU 1900 МГц, 24 ГБ GDDR6, 384 бит, 3 x DisplayPort, HDMI]',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias tarchitecto blanditiis dicta dignissimos fugiat minima minus nesciunt placeat porro sint ',
+        tags: ['Видеокарта','MSI'],
+        img: 'rx7900.jpg'
+    },
 ]
 
--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import style from './ProductCard.module.css';
 import { images } from "@/assets/images";
+import {products} from "@/assets/cardsInfo.js";
 
 const ProductCard = ({product}) => {
     return (
         <div className={style.productCard}>
             <img src={images[product.img]} alt={product.title} />
             <div className={style.productCard__text}>
-                <p>Тип: {product.type}</p>
-                <h3>Название: {product.title}</h3>
-                <p>Описание: {product.desc}</p>
+                <div className={style.productCard__textWrapp}>
+                    <h3>{product.type} {product.title}</h3>
+                </div>
+                <div className={style.productCard__textBottom}>
+                    <div className={style.productCard__tags}>
+                        {product.tags.map((tag,index) => <button key={index}>{tag} </button>)}
+                    </div>
+                    <div className={style.productCard__price}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="40" height="40" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Trash bin">
+                            <path d="M4 7h16"></path>
+                            <path d="M10 11v7"></path>
+                            <path d="M14 11v7"></path>
+                            <path d="M6 7l1 14a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2l1-14"></path>
+                            <path d="M9 7V5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2"></path>
+                        </svg>
+                        <p>{product.price}₽</p>
+
+                    </div>
+
+                </div>
             </div>
+
         </div>
     );
 };
