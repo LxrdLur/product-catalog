a4dd22327b3ff7c3f4f2d0c7a8eb85e2d0ac1db2
Nikita Parshin <Panda01r@mail.ru>
2026-02-05 19:31:16 +0300

    fix mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import FilterList from "@/components/FilterList.jsx";
-import CardsList from "@/components/CardsList.jsx";
-import {products} from "@/assets/cardsInfo.js";
-import Catalog from "@/components/Catalog.jsx";
+import CatalogPage from "@/pages/CatalogPage.jsx";
 
 export default function App() {
 
 	return (
 		<div className="app">
-			<Catalog/>
+			<CatalogPage/>
 		</div>
 	)
 }
--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ButtonFilter from "@/components/button/ButtonFilter.jsx";
 
-const FilterList = () => {
+const FilterList = ({filters}) => {
     const clickSort = (sort) => {
         console.log(sort)
     }
     return (
         <div className='filterList'>
-            {FILTERS.map((filter) => (
+            {filters.map((filter) => (
                 <ButtonFilter
                     filterName={filter.label}
                     key={filter.value}
