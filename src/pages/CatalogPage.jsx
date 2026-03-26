0ad143f62d9e3848a88d44a4a517c38541160245
Nikita Parshin <Panda01r@mail.ru>
2026-03-26 21:22:48 +0300

    wip - cart widget

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {SORT_OPTIONS} from "@/features/productSort/modal/sortConfig.js";
 import {PRODUCTS} from "@/entities/product/model/productsConfig.js";
 import Catalog from "@/widgets/catalog/UI/Catalog.jsx";
+import CartWidget from "@/widgets/cart/UI/CartWidget.jsx";
 
 const CatalogPage = () => {
     return (
         <div>
             <Catalog filters={SORT_OPTIONS} products={PRODUCTS}/>
+            <CartWidget />
         </div>
     );
 };
---
+++ src/widgets/cart/UI/CartWidget.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './CartWidget.module.css'
+
+const CartWidget = () => {
+
+    const hasCartProduct = JSON.parse(localStorage.getItem('cart')) || []
+
+    if(hasCartProduct.length > 0){
+        return (
+            <div className={styles.cartWidget}>
+
+            </div>
+        );
+    }else{
+        console.log('Корзина пуста')
+    }
+
+};
+
+export default CartWidget;
